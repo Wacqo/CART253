7c8368e65102c6a3937f18026bacb5aac419d60f
Wacqo <61024934+Wacqo@users.noreply.github.com>
2020-09-16 13:26:08 -0400

    Drawing experiment: added colour to my code

--- experiments/Drawing-experiments/template-p5-project/js/script.js
+++ experiments/Drawing-experiments/template-p5-project/js/script.js
@@ -11,7 +11,7 @@ Here is a description of this template p5 project.
 function setup() {
 createCanvas (500, 500);
 
-background (200, 0, 200);
+background (127, 0, 200);
 
 // point(50, 50);
 
@@ -20,13 +20,24 @@ background (200, 0, 200);
 
 // line (0, 0, 500, 500);
 // line (500, 0, 0, 500);
-ellipseMode(CORNER); {
-fill (0, 200, 200)
-ellipse (250, 250, 100, 100);
+ellipseMode(CORNER);
+
+fill (0, 190, 190, 100)
+//stroke (127, 0, 200);
+noStroke();
+
+ellipse (250, 250, 100, 100)
+  fill (0, 200, 200, 100);
 ellipse (250, 250, 80, 80);
+fill (0, 210, 210, 100);
 ellipse (250, 250, 60, 60);
+fill (0, 220, 220,100);
 ellipse (250, 250, 40, 40);
-}
+fill (0, 230, 230, 100);
+ellipse (250, 250, 20, 20);
+
+fill(250, 0, 0, 100);
+rect (50, 400, 60);
 }
 
 // draw()
