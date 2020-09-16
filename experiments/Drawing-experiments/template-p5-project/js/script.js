168feef6ed07ea545d72346de93291e0baf68b0f
Wacqo <61024934+Wacqo@users.noreply.github.com>
2020-09-16 12:50:10 -0400

    drawing experiment: Drew a cone

--- experiments/Drawing-experiments/template-p5-project/js/script.js
+++ experiments/Drawing-experiments/template-p5-project/js/script.js
@@ -9,7 +9,24 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+createCanvas (500, 500);
 
+background (200, 0, 200);
+
+// point(50, 50);
+
+// fill (0, 200, 200);
+ rect (50, 50, 50);
+
+// line (0, 0, 500, 500);
+// line (500, 0, 0, 500);
+ellipseMode(CORNER); {
+fill (0, 200, 200)
+ellipse (250, 250, 100, 100);
+ellipse (250, 250, 80, 80);
+ellipse (250, 250, 60, 60);
+ellipse (250, 250, 40, 40);
+}
 }
 
 // draw()
