e06e02e82d3d37d13a9feffe9b7db932d1e3817b
Wacqo <61024934+Wacqo@users.noreply.github.com>
2020-09-26 11:28:39 -0400

    updated alien

--- experiments/02-draw-an-alien/template-p5-project/js/script.js
+++ experiments/02-draw-an-alien/template-p5-project/js/script.js
@@ -24,32 +24,47 @@ let p10 = { x: 240, y: 150};
 let p11 = { x: 260, y: 150 };
 let p12 = { x: 280, y: 170 };
 
-var bubble = {
-  X: 300,
-  Y: 200,
-}
+let bubbles = [];
 
 function setup() {
 
 createCanvas (500, 500);
-background (200, 200, 200);
+background (0);
+
+for (let i = 0; i<10; i++) {
+  let x = 20 + 80 * i;
+  bubbles[i] = new Bubble(x, 400, 20);
+}
+for (let i = 0; i < bubbles.length; i++) {
+bubbles[i].show();
+}
+
 
 // Control panel frame
 rectMode(CENTER);
+//noFill ();
+fill (250);
 rect (width/2, 500 , 500, 300);
 
-rect
 
 
-//The body of the alien
+
+//The body of the alien - Dark green
+noStroke();
+fill (41, 79, 63);
 ellipse (250, 500, 400, 200);
 
-// The head of the Alien
+// The head of the Alien - lighter green
+noStroke ();
+fill (44, 115, 85);
 ellipse (250, 250, 200, 400);
 
 // The eyes of the Alien
+stroke (0);
+fill (201, 169, 169);
 ellipse (160, 250, 100, 150);
 ellipse (340, 250, 100, 150);
+fill (0, 0 , 0);
 ellipse (160, 250, 30, 30);
 ellipse (340, 250, 30, 30);
 
@@ -60,10 +75,12 @@ curve (130, 400, 110, 250, 210, 250, 230, 400);
 curve (310, 400, 290, 250, 390, 250, 410, 400);
 
 // The mouth of the Alien
+fill (77, 77, 77);
 arc (250, 335, 110, 180, 0, PI, CHORD);
 
 
 // The teeth of the alien
+fill (250, 250, 250);
 triangle (200, 335, 210, 355, 220, 335);
 triangle (220, 335, 230, 355, 240, 335);
 triangle (240, 335, 250, 355, 260, 335);
@@ -80,6 +97,7 @@ triangle (280, 335, 290, 355, 300, 335);
 
 // The curves on the top of the aliens head
 stroke(0);
+noFill ();
 curve(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y);
 curve(p5.x, p5.y, p6.x, p6.y, p7.x, p7.y, p8.x, p8.y);
 //curve(p9.x, p9.y, p10.x, p10.y, p11.x, p11.y, p12.x, p12.y);
@@ -92,5 +110,22 @@ curve(p5.x, p5.y, p6.x, p6.y, p7.x, p7.y, p8.x, p8.y);
 //
 // Description of draw() goes here.
 function draw() {
+
+}
+
+class Bubble  {
+constructor (x, y, r) {
+
+ this.x = x;
+ this.y = y;
+ this.r = r;
+}
+
+show () {
+stroke(0);
+fill (0);
+ellipse (this.x, this.y, this.r);
+
+}
 
 }
