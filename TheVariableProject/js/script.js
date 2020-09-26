d3343c3d013dffd67b5140229af90df2ea929405
Wacqo <61024934+Wacqo@users.noreply.github.com>
2020-09-26 16:21:21 -0400

    This is my first attempt on the variable project

---
+++ TheVariableProject/js/script.js
@@ -0,0 +1,22 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+background(255, 255, 255);
+
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
