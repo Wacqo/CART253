f12b2e292c706d95957f09821a3c23f028430467
Wacqo <61024934+Wacqo@users.noreply.github.com>
2020-09-28 17:04:25 -0400

    this was just playing around with variables

--- experiments/Variables/js/script.js
+++ experiments/Variables/js/script.js
@@ -8,13 +8,36 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
-function setup() {
 
+var circle1 = {
+  x: 0,
+  y: 250,
+  height: 100,
+}
+
+function setup() {
+createCanvas(500, 500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(250, 0, 250);
+
+push();
+ fill(250);
+ ellipse(circle1.x, circle1.y, circle1.height);
+//circle1.x = circle1.x + 1
+if (circle1.x < 250){
+circle1.x = circle1.x + 1
+}
+if (circle1.x >= 250) {
+  circle1.x = circle1.x - 1
+}
+
+pop();
+rectMode(CENTER);
+  rect(250, 250, 50);
 
 }
